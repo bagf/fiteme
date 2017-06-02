66c555d16a27911f2dba6244494d4bbe5e0b489a
bagf <rory@eishgaming.co.za>
2017-06-02 19:03:14 +0200

    Added handle method

--- src/Fiteme/Contracts/Http/Kernel.ts
+++ src/Fiteme/Contracts/Http/Kernel.ts
@@ -4,6 +4,7 @@ import {Response} from "./Response";
 
 export interface Kernel {
     bootstrap();
+    handle(request: Request): Response;
     terminate(request: Request, response: Response);
     getApplication();
 }
