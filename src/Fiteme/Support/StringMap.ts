d85532eaa1693d3f59ed13e86ff7578d6bb29f87
bagf <rory@eishgaming.co.za>
2017-06-03 00:31:35 +0200

    Added intersect method

--- src/Fiteme/Support/StringMap.ts
+++ src/Fiteme/Support/StringMap.ts
@@ -1,4 +1,27 @@
 
 export class StringMap extends Map<string, string|StringMap> {
     // Hax?
+
+    intersect(map: StringMap): StringMap {
+        let result = new StringMap();
+
+        for (let key in this.keys()) {
+            if (!map.has(key)) {
+                continue;
+            }
+
+            let value = this.get(key);
+            let compareValue = map.get(key);
+
+            if (value instanceof StringMap) {
+                if (compareValue instanceof StringMap) {
+                    result.set(key, value.intersect(compareValue));
+                }
+            } else if (value === compareValue) {
+                result.set(key, value);
+            }
+        }
+
+        return result;
+    }
 }
